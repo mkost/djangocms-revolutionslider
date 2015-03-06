df59a8971cbfe149feb7af5a7276d27edf229398
Maciej Kostuch <mkostuch@tdwizard.com>
2015-03-06 08:44:07 +0100

    model filefield upload_to fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "djangocms-revolutionslider",
-    version = "0.5.1",
+    version = "0.5.2",
     author = "Royendgel Silberie",
     author_email = "royendgel@techprocur.com",
     description = "Quick and easy Implementation of revolution slider",
